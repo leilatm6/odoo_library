e1c61bb0437f8a00e78ffaf6ab851e9699a93a43
Leila Talaiminaei <talaiminaei@gmail.com>
2026-07-02 15:15:26 +0400

    user complete

--- models/lib_users.py
+++ models/lib_users.py
@@ -7,4 +7,16 @@ class LibUsers(models.Model):
 
     max_books = fields.Integer(string="Maximum Books",default=5)
 
-    
+    available_borrows = fields.Integer(
+        string="Available Borrows",
+        compute="_compute_available_borrows",
+        store=True,
+    )
+
+    @api.depends("max_books", "borrowed_book_ids.state")
+    def _compute_available_borrows(self):
+        for record in self:
+            active = len(record.borrowed_book_ids.filtered(
+                lambda b: b.state == 'borrowed'
+            ))
+            record.available_borrows = record.max_books - active
