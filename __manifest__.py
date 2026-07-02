daacb831cb006b2595d26293d0babc1ba13ae0d0
Leila Talaiminaei <talaiminaei@gmail.com>
2026-07-02 14:32:34 +0400

    users add

--- __manifest__.py
+++ __manifest__.py
@@ -6,7 +6,7 @@
         'views/book_property_view.xml',
         "views/book_property_type_view.xml",
         "views/book_borrow_view.xml",
-       
+        "views/lib_users_view.xml",
         "views/library_menus.xml",
 
         'security/library_security.xml',
